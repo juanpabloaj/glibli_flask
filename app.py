e2907357883af1692d3b64b4eec33c6b87ccf536
JuanPablo <jpabloaj@gmail.com>
2020-06-28 20:08:39 -0400

    added template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,4 +7,10 @@ GLIBLI_URL = "https://ghibliapi.herokuapp.com"
 
 @app.route("/")
 def root():
-    return "hello flask"
+    movies = [
+        {
+            "title": "My Neighbor Totoro",
+            "people": ["Mei Kusakabe", "Satsuki Kusakabe", "Tatsuo Kusakabe"],
+        }
+    ]
+    return render_template("movies.html", movies=movies)
