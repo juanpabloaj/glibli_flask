55d5b8965d73a30475e2aadfa3637001c72a67c7
JuanPablo <jpabloaj@gmail.com>
2020-06-28 22:30:22 -0400

    added cache to request

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 import requests
 from movies import movies_with_people
+from cachetools import cached, TTLCache
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 app = Flask(__name__)
 
 GLIBLI_URL = "https://ghibliapi.herokuapp.com"
+
+
+@cached(cache=TTLCache(maxsize=1024, ttl=60))
+def request_data(url):
+    app.logger.info("requesting %s", url)
+    return requests.get(url).json()
 
 
 @app.route("/")
@@ -18,8 +28,8 @@ def movies():
     movies_url = GLIBLI_URL + "/films"
     people_url = GLIBLI_URL + "/people"
 
-    movies = requests.get(movies_url).json()
-    people = requests.get(people_url).json()
+    movies = request_data(movies_url)
+    people = request_data(people_url)
 
     with_people = movies_with_people(movies, people)
 
